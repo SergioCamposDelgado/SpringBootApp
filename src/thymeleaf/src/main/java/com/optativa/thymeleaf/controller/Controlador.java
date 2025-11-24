391af44eb37d697fcf948fa764f331426eb6a388
SergioCamposDelgado <campos.delgado.sergio@iescamas.es>
2025-11-24 10:13:47 +0100

    Modificado la estructura del proyecto

--- src/thymeleaf/src/main/java/com/optativa/thymeleaf/controller/Controlador.java
+++ src/thymeleaf/src/main/java/com/optativa/thymeleaf/controller/Controlador.java
@@ -1,9 +1,11 @@
-package com.optativa.thymeleaf;
+package com.optativa.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.optativa.thymeleaf.ThymeleafApplication;
 
 
 
@@ -17,7 +19,7 @@ public class Controlador {
     }
 
 	@GetMapping("/saluda")
-	public String saludo(@RequestParam(required = false, defaultValue = "Test") String name, Model modelo) {
+	public String saludo(@RequestParam(required = false, defaultValue = "Anonimo") String name, Model modelo) {
 		System.out.println("#####  Entra en /saluda");
 		//String name = "Manuel";
 		modelo.addAttribute("nombre", name);
