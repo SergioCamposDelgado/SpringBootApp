db9407a23ee5271506480df937d4e99b21c4ca6b
Pablo <pablo.dominguez@iescamas.es>
2025-11-24 11:00:56 +0100

    Ejercicio 2

--- src/thymeleaf/src/main/java/com/optativa/thymeleaf/controlador/Controlador.java
+++ src/thymeleaf/src/main/java/com/optativa/thymeleaf/controlador/Controlador.java
@@ -1,9 +1,15 @@
-package com.optativa.thymeleaf;
+package com.optativa.thymeleaf.controlador;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import com.optativa.thymeleaf.ThymeleafApplication;
+import com.optativa.thymeleaf.entidad.Producto;
 
 
 
@@ -23,5 +29,22 @@ public class Controlador {
 		modelo.addAttribute("nombre", name);
 		
 		return "saludo";
+	}
+	
+	@GetMapping("/productos")
+	public String listado(Model model) {
+	
+		
+	
+		List<Producto> listaProductos = new ArrayList<Producto>();
+		//String nombre, int precio, String categoria
+		Producto p1 = new Producto("Pan", 1, "Alimentación");
+		Producto p2 = new Producto("PC", 3000, "Componentes");
+		listaProductos.add(p1);
+		listaProductos.add(p2);
+	
+		model.addAttribute("listaProductos", listaProductos);
+		
+		return "lista";
 	}
 }
